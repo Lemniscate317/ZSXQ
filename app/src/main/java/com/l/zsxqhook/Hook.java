184f74b1b448a6ffd1b22414f27f63ad5b87937f
Eetu <eetu@irvees.com>
2019-06-26 23:37:09 +0800

    + get hidden text to show it

--- app/src/main/java/com/l/zsxqhook/Hook.java
+++ app/src/main/java/com/l/zsxqhook/Hook.java
@@ -1,6 +1,8 @@
 package com.l.zsxqhook;
 
 import android.content.Context;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -10,7 +12,10 @@ import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,6 +25,20 @@ import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class Hook implements IXposedHookLoadPackage {
+
+    public static String URLDecoderString(String str) {
+        String result = "";
+        if (null == str) {
+            return "";
+        }
+        try {
+            result = java.net.URLDecoder.decode(str, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
 
@@ -34,7 +53,94 @@ public class Hook implements IXposedHookLoadPackage {
             return;
         }
         final Class<?> topicPreviewImplClass = classLoader.loadClass("com.unnoo.quan.views.TopicPreviewImpl");
+        final Class<?> cClass = classLoader.loadClass("com.unnoo.quan.x.c");
         if (topicPreviewImplClass != null) {
+            XposedHelpers.findAndHookMethod(topicPreviewImplClass, "a", cClass, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    super.beforeHookedMethod(param);
+
+//                    Method iMethod = param.args[0].getClass().getMethod("i");
+//                    SpannableStringBuilder iSpannableStringBuilder = (SpannableStringBuilder) iMethod.invoke(param.args[0], new Object[]{});
+//                    if (iSpannableStringBuilder != null) {
+//                        log("i:" + iSpannableStringBuilder.toString());
+//                    } else {
+//                        log("i:null");
+//                    }
+//
+//                    Method fMethod = param.args[0].getClass().getMethod("f");
+//                    SpannableStringBuilder fSpannableStringBuilder = (SpannableStringBuilder) fMethod.invoke(param.args[0], new Object[]{});
+//                    if (fSpannableStringBuilder != null) {
+//                        log("f:" + fSpannableStringBuilder.toString());
+//                    } else {
+//                        log("f:null");
+//                    }
+
+//                    Field jField = param.args[0].getClass().getDeclaredField("j");
+//                    jField.setAccessible(true);
+//                    SpannableStringBuilder jSpannableStringBuilder = (SpannableStringBuilder) jField.get(param.args[0]);
+//                    if (jSpannableStringBuilder != null) {
+//                        log("i:" + jSpannableStringBuilder.toString());
+//                    } else {
+//                        log("i:null");
+//                    }
+//
+//                    Field gField = param.args[0].getClass().getDeclaredField("g");
+//                    gField.setAccessible(true);
+//                    SpannableStringBuilder gSpannableStringBuilder = (SpannableStringBuilder) gField.get(param.args[0]);
+//                    if (gSpannableStringBuilder != null) {
+//                        log("i:" + gSpannableStringBuilder.toString());
+//                    } else {
+//                        log("i:null");
+//                    }
+
+//                    Method toStringMethod = param.args[0].getClass().getMethod("toString");
+//                    String toString = (String) toStringMethod.invoke(param.args[0], new Object[]{});
+//                    if (toString != null) {
+//                        log("toString:" + toString);
+//                    } else {
+//                        log("toString:null");
+//                    }
+                }
+
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    super.afterHookedMethod(param);
+
+                    Method toStringMethod = param.args[0].getClass().getMethod("toString");
+                    String toString = (String) toStringMethod.invoke(param.args[0], new Object[]{});
+                    if (toString != null) {
+                        String[] split = toString.split(",");
+                        boolean isFound = false;
+                        String targetStr = "";
+                        for (String str : split) {
+                            //log(str);
+                            if (str.contains("text=(")) {
+                                isFound = true;
+                                continue;
+                            }
+                            if (isFound) {
+                                isFound = false;
+                                targetStr = str;
+                                //log("============"+targetStr);
+                                targetStr = URLDecoderString(targetStr);
+                                //log("============"+targetStr);
+                            }
+                        }
+
+                        Field mTextViewField = topicPreviewImplClass.getDeclaredField("mTextView");
+                        mTextViewField.setAccessible(true);
+                        TextView mTextView = (TextView) mTextViewField.get(param.thisObject);
+                        if (!TextUtils.isEmpty(targetStr)) {
+                            mTextView.setText(targetStr);
+                        }
+
+                    } else {
+                        log("toString:null");
+                    }
+                }
+            });
+
             XposedHelpers.findAndHookMethod(topicPreviewImplClass, "a", boolean.class, new XC_MethodHook() {
                 @Override
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
